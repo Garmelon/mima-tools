d0807d575aed9f37427e80d0b8b46134e7ed5dfd
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-03-29 12:38:04 +0200

    Add metadata file to Options

--- mima-run/Mima/MimaRun/Options.hs
+++ mima-run/Mima/MimaRun/Options.hs
@@ -3,18 +3,34 @@ module Mima.MimaRun.Options
   , parserInfo
   ) where
 
+import           Data.List
+import           Data.Maybe
 import           Options.Applicative
 
 data Options = Options
-  { inputFile :: FilePath
-  , steps     :: Maybe Integer
+  { inputFile    :: FilePath
+  , metadataFile :: FilePath
+  , steps        :: Maybe Integer
   } deriving (Show)
 
+createOptions :: FilePath -> Maybe FilePath -> Maybe Integer -> Options
+createOptions inFile metaFile = Options inFile resolvedMetadataFile
+  where
+    inputRenamedToMeta = removeExtension inFile ++ "mima-meta"
+    resolvedMetadataFile = fromMaybe inputRenamedToMeta metaFile
+    removeExtension = dropWhileEnd (/= '.')
+
 parser :: Parser Options
-parser = Options
+parser = createOptions
   <$> strArgument
       (  help "The .mima file to use"
       <> metavar "INPUTFILE"
+      )
+  <*> (optional . strOption)
+      ( short 'm'
+      <> long "metadata"
+      <> help "The metadata file to use"
+      <> metavar "METAFILE"
       )
   <*> (optional . option auto)
       (  short 'n'
@@ -22,6 +38,5 @@ parser = Options
       <> help "Maximum number of steps to execute"
       <> metavar "STEPS"
       )
-
 parserInfo :: ParserInfo Options
 parserInfo = info (parser <**> helper) (failureCode 1)
